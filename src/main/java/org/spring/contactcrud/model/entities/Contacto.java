6f51a98e738243f7578f6cd234c8b05401ed5d97
EliezerSA <eliezersantiago-100@outlook.com>
2025-02-17 01:33:39 -0600

    Project Crud Contacts - Se agregan libreria de lombok para reducir codigo en entities, y clase Contacto Repository en proyecto Crud Contactos

--- src/main/java/org/spring/contactcrud/model/entities/Contacto.java
+++ src/main/java/org/spring/contactcrud/model/entities/Contacto.java
@@ -1,5 +1,11 @@
 package org.spring.contactcrud.model.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
@@ -8,13 +14,30 @@ import java.io.Serializable;
  * fecha: 16/02/2025
  * Desc: Clase para las propiedades de contactos
  * **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "contacto")
 public class Contacto implements Serializable {
 
     //Propiedades
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_contacto")
     private int idContacto;
+
+    @Column(name = "nombre")
     private String nombre;
+
+    @Column(name = "apellido")
     private String apellido;
+
+    @Column(name = "direccion")
     private String direccion;
+
+    @Column(name = "telefono")
     private String telefono;
 
 }
--- src/main/java/org/spring/contactcrud/repository/ContactoRepository.java
+++ src/main/java/org/spring/contactcrud/repository/ContactoRepository.java
@@ -1,10 +1,27 @@
 package org.spring.contactcrud.repository;
 
+import org.spring.contactcrud.model.entities.Contacto;
+import org.spring.contactcrud.repository.persistence.CrudRepository;
+
+import javax.persistence.EntityManager;
+
 /**
  *
  * @author Eliezer Santiago
  * fecha: 16/02/2025
  * Desc: Clase primer repository para contactos y crear metodos CRUD
  * **/
-public class ContactoRepository {
+public class ContactoRepository extends CrudRepository<Contacto> {
+
+    //Llamada general a los metodos del CrudRepository
+    public ContactoRepository() {
+        super(Contacto.class);
+    }
+
+    //Referencia a la conexion de la BD por medio de un metodo abstracto
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
 }
